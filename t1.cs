d27bdb37f4953d4383994df6d2f3c3457dde1c4d
cordis-dev <cordis.dev@gmail.com>
2016-09-14 16:54:57 +0300

    Create t1.cs

---
+++ t1.cs
@@ -0,0 +1,13 @@
+public class Class1
+{
+	public void Test()
+	{
+		var defaultContent = "<div>content</div>";
+		var escapedContent = "&lt;div&gt;content&lt;div&gt;";
+		var escapedQoutes = "\"";
+		var defaultQoutes = "''";
+	}
+
+	public const string LtuLetters = "Aa Ąą Bb Cc Čč Dd Ee Ęę Ėė Ff Gg Hh Ii Įį Yy Jj Kk Ll Mm Nn Oo Pp Rr Ss Šš Tt Uu Ųų Ūū Vv Zz Žž";
+	public const string RuLetters = "а, о, у , ы, э, я, е, ё, ю, и; б, в, г, д, й, ж, з, к, л, м, н, п, р, с, т, ф, х, ц, ч, ш, щ;";
+}
